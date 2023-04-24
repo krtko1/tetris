4d96a5a8bf19a3140e9e9ae9d845a669519e5b33
NicolasOrsag <nicolas.orsag@gmail.com>
2023-04-24 17:14:43 +0200

    Create first itteration of game

---
+++ tetris/piece.py
@@ -0,0 +1,13 @@
+import constants
+
+
+class Piece(object):
+    rows = 20  # y
+    columns = 10  # x
+
+    def __init__(self, column, row, shape):
+        self.x = column
+        self.y = row
+        self.shape = shape
+        self.color = constants.shape_colors[constants.shapes.index(shape)]
+        self.rotation = 0  # number from 0-3
