14e4737366c8a33e386885c78f784e3fe529530e
Pavol Repisky <pal.repisky@gmail.com>
2023-04-25 14:58:44 +0200

    Create tests for create_grid and check_lost functions

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,11 +1,15 @@
 import unittest
 
 from tests.test_convert_shape_format import TestConvertShapeFormat
+from tests.test_create_grid import TestCreateGrid
+from tests.test_check_lost import TestCheckLost
 
 if __name__ == "__main__":
     suite = unittest.TestSuite()
-    suite.addTests(
-        unittest.defaultTestLoader.loadTestsFromTestCase(TestConvertShapeFormat)
-    )
+    suite.addTests([
+        unittest.defaultTestLoader.loadTestsFromTestCase(TestConvertShapeFormat),
+        unittest.defaultTestLoader.loadTestsFromTestCase(TestCreateGrid),
+        unittest.defaultTestLoader.loadTestsFromTestCase(TestCheckLost)
+    ])
 
     unittest.TextTestRunner().run(suite)
